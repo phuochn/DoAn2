25dd5c1931c13e7b7ec656cd18b0aac704b965d6
phuochn <48937887+phuochn@users.noreply.github.com>
2019-07-15 22:52:39 +0700

    Update TV_DS1307.c

--- TV_DS1307.c
+++ TV_DS1307.c
@@ -8,19 +8,16 @@ void dat_lai_thoi_gian_ds1307()
    i2c_write (phut_ds1307) ;
    i2c_write (gio_ds1307);
    i2c_stop () ;
-}
+}                                  
 void doc_thoi_gian_ds1307()
 {
    i2c_start () ;
    i2c_write (0xD0) ;
    i2c_write (0x00);
-   i2c_start ();
+   i2c_start ();      
    i2c_write (0xD1) ;
    giay_ds1307 = i2c_read ();
    phut_ds1307 = i2c_read ();
    gio_ds1307 = i2c_read (0);
    i2c_stop () ;
 }
-
-
-
